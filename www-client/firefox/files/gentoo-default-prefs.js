7adc472f4229b19073a2e393902814252556b6f4
stefson <herrtimson@yahoo.de>
2024-07-21 12:45:07 +0200

    www-client/firefox: add kill switch

--- www-client/firefox/files/gentoo-default-prefs.js
+++ www-client/firefox/files/gentoo-default-prefs.js
@@ -10,3 +10,5 @@ pref("intl.locale.requested",              "");
 pref("network.trr.mode",                   5);
 /* Disable use of Mozilla Normandy service by default */
 pref("app.normandy.enabled",               false);
+/* Disable website advertising preferences by default */
+pref("dom.private-attribution.submission.enabled",  false);
